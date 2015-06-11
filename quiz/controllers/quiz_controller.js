03fbe2c8ca1cbd63fc4e71f40365e0e896ae0cc9
addu0g <addu0g@live.com.mx>
2015-06-10 20:25:21 -0500

    Autores agregados

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -9,3 +9,4 @@ exports.answer = function(req,res){
 		res.render('quizes/answer', {respuesta:'Incorrecto'});
 	}
 }
+
--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author',function(req, res){
+	res.render('author', {author: 'Jos&eacute; Edgar G&oacute;mez Elizalde'});
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
